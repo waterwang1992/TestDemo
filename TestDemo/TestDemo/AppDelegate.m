580768a9a9dc4de88bfbc3294f1ed06a56c17e44
Clarence <1225379577@qq.com>
2017-08-15 13:05:21 +0800

    log dev1

--- TestDemo/TestDemo/AppDelegate.m
+++ TestDemo/TestDemo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"dev1");
+    NSLog(@"dev1");
     return YES;
 }
 
