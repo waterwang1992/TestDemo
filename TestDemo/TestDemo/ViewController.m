9e8c434778da404d052d8588cbefb01d44095372
Clarence <1225379577@qq.com>
2017-08-15 11:14:59 +0800

    another dev1

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"i am dev1");
+    NSLog(@"i am dev1");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
