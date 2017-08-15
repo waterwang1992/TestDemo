3c502917ea82edc5c410fb6691a222386e9d47d2
Clarence <1225379577@qq.com>
2017-08-15 10:52:01 +0800

    creat testDemo project

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by wangcongcong on 2017/8/15.
+//  Copyright © 2017年 wangcongcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by wangcongcong on 2017/8/15.
+//  Copyright © 2017年 wangcongcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
